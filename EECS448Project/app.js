5a5c07322c90996b505212ab045ff6ae0090fbcf
Jeffery Anderson <JefferyMorganAnderson@gmail.com>
2017-02-21 16:34:31 -0600

    Changes to pages to include links to other pages.
    Changes to App.js to access owner and use pages.

--- EECS448Project/app.js
+++ EECS448Project/app.js
@@ -5,6 +5,14 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + "/Pages/" + "homepage.html");
 });
 
+app.get('/owner',function(req,res){
+    res.sendFile(__dirname + "/Pages/" + "ownerpage.html");
+});
+
+app.get('/user',function(req,res){
+    res.sendFile(__dirname + "/Pages/" + "userpage.html");
+});
+
 app.listen(3000, function(){
     console.log('EECS 448 project app listening on port 3000');
 })
