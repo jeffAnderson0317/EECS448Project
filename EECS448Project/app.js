daa467f2dad2725625cb47f39ecb6ac849ca807d
Jeffery Anderson <JefferyMorganAnderson@gmail.com>
2017-03-05 11:06:14 -0600

    Documentation for app.js file

--- EECS448Project/app.js
+++ EECS448Project/app.js
@@ -1,18 +1,22 @@
 var express = require("express");
 var app = express();
 
+//Get homepage and send it to whoever is requesting the page.
 app.get('/', function(req,res){
     res.sendFile(__dirname + "/Pages/" + "homepage.html");
 });
 
+//Get owner page and send it to whoever is requesting the page.
 app.get('/owner',function(req,res){
     res.sendFile(__dirname + "/Pages/" + "ownerpage.html");
 });
 
+//Get user page and send it to whoever is requesting the page.
 app.get('/user',function(req,res){
     res.sendFile(__dirname + "/Pages/" + "userpage.html");
 });
 
+//Get listen for any requests on port 3000.
 app.listen(3000, function(){
     console.log('EECS 448 project app listening on port 3000');
 })
